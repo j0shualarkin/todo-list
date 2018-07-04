4bab98b95e667ca115899ee14430943cb96f93c5
Joshua Larkin <joslarki@iu.edu>
2018-07-04 13:00:40 -0400

    collecting terms and types

--- TC for TR.rkt
+++ TC for TR.rkt
@@ -2,7 +2,7 @@
 
 
 
-(define-type TypeEnv (Listof (Pair Symbol Symbol)))
+(define-type TypeEnv (Listof (Pairof Symbol Symbol)))
 (define-type Id Symbol)
 (define-type Type Symbol)
 
@@ -19,17 +19,41 @@
 (extend '((x . Nat) (y . Nat)) 'z 'String)
 
 
+
+
+
 (: lookup (-> Id TypeEnv Type))
 ;; lookup returns the type that the given identifier maps to
 ;; an id not seen before will produce an error
 (define (lookup x Γ)
-  (match Γ
-    [(cons (cons x0 t0) rest) (if (eqv? x0 x) t0 (lookup x rest))]
-     ;; (? add an (or _ (alias? x0 x)))
-    [_ (error "oops")]))
+  (let ([maybe-type : (U (Pairof Symbol Symbol) False) (assoc x Γ)])
+    (if maybe-type (cdr maybe-type)
+        (error 'lookup "could not find a type for ~a in ~a" x Γ))))
+
+(: find-+ (-> Type Type Type))
+;; helper function to findtype
+;; (+ n1 n2) is a Number when n1 is a Number and n2 is a Number
+(define (find-+ t1 t2)
+  (match* (t1 t2)
+    [('Number 'Number) 'Number]
+    [(_ _) (error '+ "expects two Numbers got: ~a ~a" t1 t2)]))
+
+(: findtype (-> Any TypeEnv Type))
+;; takes a program and returns its type
+;; traverses program and builds a type environment of all terms 
+(define (findtype e Γ)
+  (match e
+    [(? symbol? x) (lookup x Γ)]
+    [(? number? x) 'Number]
+    ;[`(: ,x ,t) (extend Γ x t)]
+    ;[`(let ([,id : ,t ,v]) ,b) (findtype b (extend Γ id t))]
+    [`(+ ,n1 ,n2) (find-+ (findtype n1 Γ) (findtype n2 Γ))]))
 
 
 
+
+
+#;#;#;
 (: typecheck (-> Expr TypeEnv
                  Type))
 
